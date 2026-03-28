8f1014346ee463abf4ee33f7b5cf33db8f4c7b84
fabiosilvaresjr <fabioasilvares@gmail.com>
2026-03-27 23:26:54 -0300

    feat: adiciona script .bat e creditos no PDF

--- INICIAR_SISTEMA.bat
+++ INICIAR_SISTEMA.bat
@@ -1,12 +1,20 @@
 @echo off
-title Sistema Alimentuus
+title Servidor Alimentuus
 color 0A
-echo ===================================================
-echo   ALIMENTUUS - GERADOR DE CATALOGO
-echo   Iniciando o servidor... Por favor, aguarde.
-echo   AVISO: Nao feche esta janela preta enquanto usa o site!
-echo ===================================================
-timeout /t 3 >nul
+
+cd /d "%~dp0"
+
+echo ==========================================
+echo    ALIMENTUUS - GERADOR DE CATALOGO
+echo ==========================================
+echo.
+echo Iniciando o servidor... Por favor, aguarde.
+echo AVISO: Nao feche esta janela preta enquanto usa o site!
+echo.
+
+:: Abre o navegador padrao direto no site
 start http://localhost:3000
-node index.js
+
+npm start
+
 pause
